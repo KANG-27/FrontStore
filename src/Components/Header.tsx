edd0932b6645e1ebc1ecfef906f685ac4d9bd52b
Kevin Niño <kevinandresrg@gmail.com>
2025-02-14 11:50:22 -0500

    final

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { fetchData } from "../api/api";
+import Presupuesto from "./Presupuesto";
 
 interface Producto {
   id: number;
@@ -13,6 +14,10 @@ function Header() {
   // const [loading, setLoading] = useState(true);
   const [productos, setProductos] = useState<Producto[]>([]);
   const [modal, setModal] = useState(false);
+  const [presupuesto, setPresupuesto] = useState(false);
+  const [listPresupuesto, setListPresupuesto] = useState(false);
+  const [price, setPrice] = useState();
+
   const carritoCantidad =
     JSON.parse(localStorage.getItem("carritoCantidad")) || 0;
 
@@ -28,6 +33,18 @@ function Header() {
     getProductos();
   }, []);
 
+  function findBestCombination(price: string) {
+    const getProduct = async () => {
+      const data = await fetchData(`http://localhost:3000/budget/${price}`);
+      if (data) {
+        setListPresupuesto(data);
+      }
+    };
+    getProduct();
+  }
+
+  console.log(listPresupuesto);
+
   return (
     <>
       <nav className="w-full px-20 py-10 bg-gray-200 text-black">
@@ -39,8 +56,13 @@ function Header() {
               <input type="text" className="border-2 border-black rounded-sm" />
             </label>
           </li>
-          <li>About Us</li>
-          <li>
+          <li
+            className="cursor-pointer"
+            onClick={async () => await setPresupuesto(true)}
+          >
+            Presupuesto
+          </li>
+          <li className="cursor-pointer">
             <div className="relative">
               {carritoCantidad > 0 && (
                 <div className="absolute bg-white top-[-10px] rounded-full flex justify-center p-1 px-2 text-[10px] right-[-7px]">
@@ -57,33 +79,82 @@ function Header() {
           </li>
         </ul>
       </nav>
+      {presupuesto && (
+        <>
+          <div className="flex flex-col items-center w-auto px-20 py-5 mt-20 border-2 border-white rounded-lg gap-5">
+            <h2>¿Cual es tu presupuesto?</h2>
+            <div className="flex flex-col gap-5">
+              <label htmlFor="">
+                Total
+                <input
+                  type="text"
+                  className="border-2 border-white ml-2 pl-2 rounded-lg"
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+              </label>
+              <button onClick={async () => await findBestCombination(price)}>
+                Buscar
+              </button>
+            </div>
+          </div>
+          {listPresupuesto && (
+            <div className="grid grid-cols-4 gap-4 mt-10">
+              {listPresupuesto.map((producto) => (
+                <div className="flex flex-col justify-between text-black bg-white border-2 border-black rounded-lg">
+                  <div className="flex justify-center items-center h-[60%]">
+                    <img
+                      src={producto.imagen}
+                      alt=""
+                      width={200}
+                      className="h-100px"
+                    />
+                  </div>
+                  <div className="flex flex-col justify-between p-5 gap-2">
+                    <h2>{producto.titulo}</h2>
+                    <span>{producto.descripcion}</span>
+                    <span>{producto.precio}</span>
+                    <button
+                      className="text-white"
+                      // onClick={async () => await agregarCarrito(producto.id)}
+                    >
+                      Agregar al carrito
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+        </>
+      )}
       {modal && (
         <div className="fixed overflow-x-auto scroll-x flex justify-center items-center w-screen h-screen bg-black/80 rounded-sm">
           <div className="flex bg-white w-[50%] flex flex-col text-black px-5 pb-10 absolute top-[20vh]">
             <div className="flex justify-between">
               <span className="text-[30px] mt-10">Carrito</span>
               <span
-                className="text-[30px]"
+                className="text-[30px] cursor-pointer"
                 onClick={async () => await setModal(false)}
               >
                 x
               </span>
             </div>
             <div className="mt-5">
-              {productos.map((product)=>(
-              <div className="flex">
-                <img src={product.imagen} alt="" width={200} className="h-[200px]"
-                />
-                <div className="flex flex-col justify-between">
-                  <h2>{product.titulo}</h2>
-                  <h2>{product.descripcion}</h2>
-                  <h2 className="text-center">{product.precio}</h2>
-                  <button className="text-white">comprar</button>
+              {productos.map((product) => (
+                <div className="flex">
+                  <img
+                    src={product.imagen}
+                    alt=""
+                    width={200}
+                    className="h-[200px]"
+                  />
+                  <div className="flex flex-col justify-between">
+                    <h2>{product.titulo}</h2>
+                    <h2>{product.descripcion}</h2>
+                    <h2 className="text-center">{product.precio}</h2>
+                    <button className="text-white">comprar</button>
+                  </div>
                 </div>
-              </div>
-
               ))}
-              
             </div>
           </div>
         </div>
